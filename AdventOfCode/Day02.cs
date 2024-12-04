ee3616e24d4bb6daa76631e2cc52adb08087c416
Sam Miller <23262411+s-mil@users.noreply.github.com>
2024-12-03 20:22:40 -0600

    day02 complete

--- AdventOfCode/Day02.cs
+++ AdventOfCode/Day02.cs
@@ -21,12 +21,12 @@ public class Day02 : BaseDay
     public override ValueTask<string> Solve_2() => new($"{NumberOfSafeZones(true)}");
 
 
-/// <summary>
-/// Process an array of numbers (A report to determine if it follows the rules)
-/// </summary>
-/// <param name="report"></param>
-/// <param name="problemDampener">If set, then ignore one potential problematic value in a report</param>
-/// <returns>True if safe</returns>
+    /// <summary>
+    /// Process an array of numbers (A report to determine if it follows the rules)
+    /// </summary>
+    /// <param name="report"></param>
+    /// <param name="problemDampener">If set, then ignore one potential problematic value in a report</param>
+    /// <returns>True if safe</returns>
     private bool IsZoneSafe(int[] report, bool problemDampener = false)
     {
         var initDirection = int.Sign(report[1] - report[0]);
@@ -39,13 +39,15 @@ public class Day02 : BaseDay
             if (dir != 0 && dir == initDirection && 1 <= Math.Abs(testValue) && Math.Abs(testValue) <= 3)
                 continue;
 
-            return problemDampener && 
-            (IsZoneSafe(report.Where((_, idx) => idx != i).ToArray()) || IsZoneSafe(report.Where((_, idx) => idx != i + 1).ToArray()));
+            return problemDampener &&
+            (
+                IsZoneSafe(report.Where((_, idx) => idx != i).ToArray()) || IsZoneSafe(report.Where((_, idx) => idx != i + 1).ToArray())
+                || IsZoneSafe(report.Where((_, idx) => idx != i - 1).ToArray()));
         }
         return true;
     }
 
-    private int NumberOfSafeZones(bool problemDampener = false) => 
+    private int NumberOfSafeZones(bool problemDampener = false) =>
         _input.Select(report => IsZoneSafe(Process(report), problemDampener))
         .Count(isSafe => isSafe);
 
